831e1f2e0dfea31db5aeedec8e297b8932bc6212
Tuan <tmtran38@uh.edu>
2022-01-02 18:28:58 -0600

    Adding Mock

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,4 +1,4 @@
-from scripts.helpful_scripts import get_account
+from scripts.helpful_scripts import get_account, get_contract
 from brownie import PenwuinToken, TokenFarm, network, config
 
 from web3 import Web3
@@ -20,6 +20,17 @@ def deploy_token_farm_and_dapp_token():
     )
 
     tx.wait(1)
+    # Penwuin token, weth_token, fau_token/dai
+    weth_token = get_contract("weth_token")
+    fau_token = get_contract("fau_token")
+
+    add_allowed_tokens(
+        token_farm,
+    )
+
+
+def add_allowed_tokens(token_farm, dict_of_allowed_tokens, account):
+    pass
 
 
 def main():
